4b71b03654e3f4258c75ed2f896d80201ffcf02c
NavyaPodili <160600065+NavyaPodili@users.noreply.github.com>
2024-05-18 16:37:17 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import AppClass from "./AppClass";
-
+import AppClass from "./AppClass";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
-    {/* change the above line - for interchaning between App and AppClass */}
+    {/* Change the following line to interchange between App and AppClass */}
+    <AppClass />
+    {/* <App /> */}
   </React.StrictMode>
 );
